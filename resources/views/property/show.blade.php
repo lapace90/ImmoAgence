197c9d6aa552e324515efac8d9ff705a3288f7e6
Ilaria <lapace90@gmail.com>
2024-05-21 02:12:04 +0200

    flash ok

--- resources/views/property/show.blade.php
+++ resources/views/property/show.blade.php
@@ -11,50 +11,50 @@
         <div class="mt-4">
             <h4>Intéressé par ce bien ?</h4>
 
-            @include('shared.flash')
 
-            <form action="{{ route('property.contact', $property) }}" method="post" class="vstack gap-3">
-                @csrf
-                <div class="row">
+            @if (session('success'))
+                @include('shared.flash')
+            @else
+                @include('shared.flash')
+                <form action="{{ route('property.contact', $property) }}" method="post" class="vstack gap-3">
+                    @csrf
+                    <div class="row">
+                        @include('shared.input', [
+                            'class' => 'col',
+                            'label' => 'Prénom',
+                            'name' => 'firstname',
+                        ])
+                        @include('shared.input', [
+                            'class' => 'col',
+                            'label' => 'Nom',
+                            'name' => 'lastname',
+                        ])
+                    </div>
+                    <div class="row">
+                        @include('shared.input', [
+                            'class' => 'col',
+                            'label' => 'Téléphone',
+                            'name' => 'phone',
+                        ])
+                        @include('shared.input', [
+                            'type' => 'email',
+                            'class' => 'col',
+                            'label' => 'Email',
+                            'name' => 'email',
+                        ])
+                    </div>
                     @include('shared.input', [
+                        'type' => 'textarea',
                         'class' => 'col',
-                        'label' => 'Prénom',
-                        'name' => 'firstname',
-                        
+                        'label' => 'Votre message',
+                        'name' => 'message',
                     ])
-                    @include('shared.input', [
-                        'class' => 'col',
-                        'label' => 'Nom',
-                        'name' => 'lastname',
-                       
-                    ])
-                </div>
-                <div class="row">
-                    @include('shared.input', [
-                        'class' => 'col',
-                        'label' => 'Téléphone',
-                        'name' => 'phone',
-                        
-                    ])
-                    @include('shared.input', [
-                        'type' => 'email',
-                        'class' => 'col',
-                        'label' => 'Email',
-                        'name' => 'email',
-                        
-                    ])
-                </div>
-                @include('shared.input', [
-                    'type' => 'textarea',
-                    'class' => 'col',
-                    'label' => 'Votre message',
-                    'name' => 'message',
-                    
-                ])
-                <div>
-                    <button class="btn btn-primary">Nous contacter</button>
-                </div>
-            </form>
+                    <div>
+                        <button class="btn btn-primary">Nous contacter</button>
+                    </div>
+                </form>
+            @endif
+
         </div>
 
         <div class="mt-4">
